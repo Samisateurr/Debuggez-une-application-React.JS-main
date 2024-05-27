6baa6dd2f7fa39890ce9fc25d64e2a63d70dc337
Samisateurr <samuel.baranowski@orange.fr>
2024-05-27 19:59:42 +0200

    fix : section "notre derniére prestation" fonctionelle

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,8 +12,10 @@ import Form from "../../containers/Form";
 import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
+
 const Page = () => {
-  const {last} = useData()
+  const {data} = useData();
+  const last=data?.events[0]
   return <>
     <header>
       <Menu />
